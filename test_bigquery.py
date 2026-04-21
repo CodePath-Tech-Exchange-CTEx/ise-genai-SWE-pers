d707e9e160b55dd8a997062261487b1b408e319d
6uan <6uannn@gmail.com>
2026-04-20 20:05:00 -0400

    fix: correct BigQuery client call and table name in test_bigquery.py

--- test_bigquery.py
+++ test_bigquery.py
@@ -1,9 +1,9 @@
 from google.cloud import bigquery
 
 
-client = bigquery.client(projects="juan-gomez-fiu")
+client = bigquery.Client(project="juan-gomez-fiu")
 
-query = "SELECT * FROM `juan-gomez-fiu.SWEpers.posts"
+query = "SELECT * FROM `juan-gomez-fiu.SWEpers.Posts`"
 results = client.query(query).result()
 
 for row in results:
